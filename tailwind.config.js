533fabfee5bdc70f5f28135f3834e812a70cfd49
saydullo5950 <saydullo5950@gmail.com>
2024-03-22 19:05:33 +0400

    scalar

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brend: {
+          blue: {
+            100: '#5F7AFF',
+            200: '#1A253C',
+            300: '#2E3F63',
+            400: '#848D9B'
+          },
+          gray: {
+            100: '#F2F3FC'
+          },
+          primary: {
+            100: '#F8F9FD'
+          }
+        }
+      }
+    },
+  },
+  gins: [],
+}
